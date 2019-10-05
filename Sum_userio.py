496397b6da9d62fdcca46c79ffd7977b8843602e
Vishnu Sekhar <er.vishnusekhar@outlook.com>
2019-10-05 16:26:05 +0400

    minor changes

---
+++ Sum_userio.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 11:43:59 2019
+
+@author: ErVis
+"""
+
+#Sum of two numbers while input being givn by the user
+num1 = input('enter first number: ')
+num2 = input('enter second number: ')
+#Calculating the sum
+sum = 
